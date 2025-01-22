5c95d596c958a855dbd6ba080e516a7179bb71ff
hellosiyam <hellosiyambd@gmail.com>
2025-01-22 21:17:40 +0600

    ES6-more

---
+++ filter.js
@@ -0,0 +1,18 @@
+// const hight = [10, 12, 16, 15, 28, 17, 28, 96, 10];
+// const sum = hight.filter(hig => hig <  20);
+// console.log(sum);
+
+// const hight = [10, 12, 16, 15, 28, 17, 28, 96, 10];
+// const sum = hight.filter(hig => hig % 2 ===1);
+// console.log(sum);
+
+// const nameof = ['siyam', 'rohul', 'asif', 'nabil', 'abir'];
+// const sum = nameof.filter(hig => hig.length > 4);
+// console.log(sum);
+
+// const hight = [10, 12, 16, 15, 28, 17, 28, 96, 10];
+// const find = hight.find(num => num > 15);
+// console.log(find);
+
+
+
---
+++ reduse.js
@@ -0,0 +1,31 @@
+// const number = [10, 12, 15, 26, 90];
+// const sum = number.reduce((first, last)=> first + last , 0)
+// console.log(sum);
+
+const array =[
+    {name:'mac', price: 150000,},
+    {name:'LENOVO', price: 120000,},
+    {name:'Hp', price: 110000,},
+    {name:'dell', price: 90000,}
+];
+
+// Map:
+const map = array.map(nam => nam.name);
+// console.log(map);
+
+// map:
+const filter1 = array.map(nam => nam.price);
+// console.log(filter1);
+
+// foeich:
+// array.forEach(p => console.log(p.price));
+
+// Reduce:
+const priceElement = array.filter(p => p.price >100000);
+console.log(priceElement);
+
+
+
+const price = array.reduce((first, last)=> first + last.price,0);
+console.log(price);
+
