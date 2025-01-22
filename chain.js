55e593428cec2253c44e040bfd5ccccbbe07a828
hellosiyam <hellosiyambd@gmail.com>
2025-01-22 18:27:01 +0600

    ES6-more

---
+++ chain.js
@@ -0,0 +1,30 @@
+const user1 =[{name:'siyam', id:777, age:24, form:'Munshigonj'}];
+// console.log(user1[0].age);
+
+
+const user2 = {
+    count : 2500,
+    date : [
+        {
+            janu: 25,
+            feb : 20,
+        }
+    ],
+    address : 'dhaka',
+}
+// console.log(user2.date[0].feb);
+
+const user3 = {
+    name :'raj',
+    presnt:{
+        address : {
+            live: 'Dhaka',
+            street : 'mohammedpour',
+        },
+    },
+}
+console.log(user3 . presnt . address . present?. live);
+console.log(user3 . presnt . address . live);
+
+
+
